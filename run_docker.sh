2ca4953eb22cc1619a5ecff1ade36d0b9379402b
Jun <dndud19999@gmail.com>
2024-04-18 15:03:49 +0900

    run_docker.sh sudo command delete

--- run_docker.sh
+++ run_docker.sh
@@ -1,5 +1,5 @@
 echo killing old docker processes
-sudo docker-compose rm -fs
+docker-compose rm -fs
 
 echo building docker containers as daemon
-sudo docker-compose up --build -d
+docker-compose up --build -d
