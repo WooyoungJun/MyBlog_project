d8c66730ac395cd733cf4e0c6e7017fe6a870e31
WooyoungJun <121923402+WooyoungJun@users.noreply.github.com>
2024-04-27 22:45:00 +0900

    Add files via upload

---
+++ flask_app/tests/test_config.py
@@ -0,0 +1,14 @@
+import os
+
+class TestConfig():
+    BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+    BASE_DB_NAME = 'test.db'
+
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, BASE_DB_NAME))
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    WTF_CSRF_ENABLED = False
+
+    SECRET_KEYS = {
+        'DEVELOPMENT_SECRET_KEY': 'test',
+        'PRODUCTION_SECRET_KEY': 'test'
+    }
